7825e1ab9395d1534cd4088773e3557b6b5def61
Sankarshan Purkayastha <sankarshan@Sankarshans-MacBook-Pro.local>
2014-05-24 04:34:43 +0530

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Sankarshan Purkayastha on 24/05/14.
+//  Copyright (c) 2014 Sankarshan Purkayastha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ Matchismo/CardGameViewController.m
@@ -0,0 +1,29 @@
+//
+//  CardGameViewController.m
+//  Matchismo
+//
+//  Created by Sankarshan Purkayastha on 24/05/14.
+//  Copyright (c) 2014 Sankarshan Purkayastha. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface CardGameViewController ()
+
+@end
+
+@implementation CardGameViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
